115d672d7b7ef3e76276a3765644905caeef2418
Echo-Sentinel <hhsnzad669@gmail.com>
2025-04-07 20:48:01 +0400

    Get Date

---
+++ 16-DateOptionalNullishDestructing/assets/js/script.js
@@ -0,0 +1,21 @@
+// task1
+// const now = new Date();
+// if (now.getMinutes() < 10) {
+//   alert(now.getHours() + ":" + "0" + now.getMinutes());
+// }else {
+//   alert(now.getHours() + ":" + now.getMinutes());
+// }
+
+// task2
+const employee = {
+    name: "Farid Ali",
+    department: "Engineering",
+    contact: {
+      email: "farid.ali@example.com",
+      phone: "555-1234",
+      emergencyContact: {
+        name: "Far For",
+        relation: "Spouse"
+      }
+    }
+  };
