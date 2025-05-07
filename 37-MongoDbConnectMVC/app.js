c63ce60de1ae35f54283f6139e29b2b31358c0ba
Echo-Sentinel <hhsnzad669@gmail.com>
2025-05-08 01:14:01 +0400

    MongoDbConnectMVC

---
+++ 37-MongoDbConnectMVC/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import productRoutes from "./src/routes/productRoutes.js";
+
+dotenv.config();
+const app = express();
+connectDB();
+
+app.use(express.json());
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
